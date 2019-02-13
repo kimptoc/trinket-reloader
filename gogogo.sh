b24671fb87cf2aa52ee79efecc984bc6486a6d03
Chris Kimpton <chris@kimptoc.net>
2019-02-13 07:47:57 +0000

    more todo

--- gogogo.sh
+++ gogogo.sh
@@ -1,9 +1,30 @@
+#!/bin/bash
 
-echo "Trinket Reloader Running!"
+function log
+{
+    echo "`date`:$1"
+}
+
+
+log "Trinket Reloader Running!"
+
+config=$1
+if [ -z "$config"]; then
+  log "ERROR - no yaml config file/url specified"
+  exit 1
+fi
+
+log "config file is yaml format, list of student names and shared trinket project ids"
 
 while true; do
-   echo "TODO - get config file"
-   echo "TODO - if end token in it stop"
-   echo "TODO - if sleep time in it, use that"
+   log "TODO - get config file"
+   curl $config > config.yaml
+   log "TODO - if end token in it stop"
+   log "TODO - get trinket project json for each in config"
+   log "TODO - for last 3(?config) run in random order"
+   log "TODO - run by generating a script which is then called"
+   log "TODO - if sleep time in it, use that"
+
    sleep 5
+   log "TODO - git pull this project to update itself - hence can change a little while running"
 done
