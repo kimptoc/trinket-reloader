9162cab9ff6cf77de6c8743b511239bea2643aed
Chris Kimpton <chris@kimptoc.net>
2019-02-13 06:38:52 +0000

    todo's

---
+++ gogogo.sh
@@ -0,0 +1,9 @@
+
+echo "Trinket Reloader Running!"
+
+while true; do
+   echo "TODO - get config file"
+   echo "TODO - if end token in it stop"
+   echo "TODO - if sleep time in it, use that"
+   sleep 5
+done
