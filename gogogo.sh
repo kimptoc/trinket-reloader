ec9c648e3506f8f48a337efbda188b48b49f0b81
Chris Kimpton <chris@kimptoc.net>
2019-02-13 09:58:16 +0000

    ruby...

--- gogogo.sh
+++ gogogo.sh
@@ -5,11 +5,16 @@ function log
     echo "`date`:$1"
 }
 
+function run_ruby
+{
+    # java -jar lib/jruby-complete-9.2.6.0.jar "$@"
+    ruby "$@"
+}
 
 log "Trinket Reloader Running!"
 
 config=$1
-if [ -z "$config"]; then
+if [ -z "$config" ]; then
   log "ERROR - no yaml config file/url specified"
   exit 1
 fi
@@ -17,10 +22,13 @@ fi
 log "config file is yaml format, list of student names and shared trinket project ids"
 
 while true; do
-   log "TODO - get config file"
+   log "Getting config file: $config"
    curl $config > config.yaml
+   log "CONFIG:"
+   cat config.yaml
    log "TODO - if end token in it stop"
    log "TODO - get trinket project json for each in config"
+   run_ruby src/check_projects.rb
    log "TODO - for last 3(?config) run in random order"
    log "TODO - run by generating a script which is then called"
    log "TODO - if sleep time in it, use that"
---
+++ src/check_projects.rb
@@ -0,0 +1,1 @@
+puts "check projects!"
